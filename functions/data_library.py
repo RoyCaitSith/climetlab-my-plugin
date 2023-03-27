f6293d3442c3f15e810c0bb83a1d141235244340
RoyCaitSith <lzfchfthdl@gmail.com>
2023-03-26 19:49:56 -0600

    update

--- functions/data_library.py
+++ functions/data_library.py
@@ -11,6 +11,16 @@ data_library[('/05_TROPICS/05_cycling_da', 'Ida', 'TROPICS_T_CLR_ENS')] = {
     'history_interval': 6,
     'hwrf_header': 'hwrf.18x18.AL092021.2021082606',
 }
+data_library[('/02_GOES_Bias_Correction/27_ChCor', 'Laura', 'NPB')] = {
+    'itime': (2020, 8, 24,  0, 0, 0),
+    'forecast_hours': 54,
+    'dir_exp': '/uufs/chpc.utah.edu/common/home/zpu-group16/cfeng/02_GOES_Bias_Correction/27_ChCor',
+    'dir_scratch': '/scratch/general/nfs1/u1237353',
+    'da_domains': ['d01'],
+    'forecast_domains': ['d01', 'd02'],
+    'cycling_interval': 6,
+    'history_interval': 6,
+}
 data_library[('/02_GOES_Bias_Correction/27_ChCor', 'Laura', 'NPB_ASRBC4CLD_CLD_ALL')] = {
     'itime': (2020, 8, 24,  0, 0, 0),
     'forecast_hours': 54,
