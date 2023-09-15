298c958a744a4a466af9e895869160d5424e34f8
RoyCaitSith <lzfchfthdl@gmail.com>
2023-09-15 13:07:27 -0600

    update

--- tests/halo2.py
+++ tests/halo2.py
@@ -4,4 +4,4 @@ n_exp_name = 1
 wrf_extract_HALO(data_library_names=['cpex']*n_exp_name,
                  dir_cases=['/08_CPEX/01_CV_RF07_AEW06_IAN']*n_exp_name,
                  case_names=['Ian']*n_exp_name,
-                 exp_names=['HALO_OE15'])
+                 exp_names=['HALO_OE25'])
