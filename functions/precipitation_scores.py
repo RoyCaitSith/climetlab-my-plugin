c42cfa4e19a8651b813e891f1aae8138ee6ea8e9
RoyCaitSith <lzfchfthdl@gmail.com>
2023-12-05 14:59:29 -0700

    update

--- functions/precipitation_scores.py
+++ functions/precipitation_scores.py
@@ -1,4 +1,5 @@
 import os
+import math
 import importlib
 import subprocess
 import numpy as np
@@ -17,10 +18,12 @@ from IPython.display import display
 
 def ETS_6h(data_library_names, dir_cases, case_names, exp_names,
            observations=['IMERG', 'CMORPH', 'GSMaP'],
+           resolutions=[12, 4],
            thresholds=[1.0, 5.0, 10.0, 15.0],
-           region_types=['domain', 'tc']):
+           region_types=['tc', 'domain']):
 
     time_interval = 6
+    tc_box_range = 500
 
     for idc in tqdm(range(len(dir_cases)), desc='Cases', unit='files', bar_format="{desc}: {n}/{total} files | {elapsed}<{remaining}"):
 
@@ -39,12 +42,13 @@ def ETS_6h(data_library_names, dir_cases, case_names, exp_names,
         initial_time = datetime(*itime)
 
         dir_weather_map = os.path.join(dir_exp, 'weather_map')
+        dir_best_track = os.path.join(dir_exp, 'track_intensity', 'best_track')
         dir_score = os.path.join(dir_exp, 'score')
         dir_ETS_6h = os.path.join(dir_score, 'ETS_6h')
         os.makedirs(dir_score, exist_ok=True)
         os.makedirs(dir_ETS_6h, exist_ok=True)
 
-        for dom in tqdm(da_domains, desc='Domains', position=0, leave=True):
+        for idd, dom in tqdm(enumerate(da_domains), desc='Domains', position=0, leave=True):
 
             columns_lists = ['Observation', 'DA_Cycle', 'Forecast_Hour', 'Date_Time', 'Threshold', 'Region_type', 'ETS']
             df = pd.DataFrame(columns=columns_lists)
@@ -77,15 +81,59 @@ def ETS_6h(data_library_names, dir_cases, case_names, exp_names,
                             wrfanl_times = wrfanl_ncfile.variables['time'][:]
                             idt = np.where(wrfanl_times == var_time)[0][0]
                             wrfanl_var = wrfanl_ncfile.variables['rain_6h'][idt,:,:]
-                            wrfanl_lat = wrfanl_ncfile.variables['lat'][idt,:,:]
-                            wrfanl_lon = wrfanl_ncfile.variables['lon'][idt,:,:]
+                            wrfanl_lat = wrfanl_ncfile.variables['lat'][:,:]
+                            wrfanl_lon = wrfanl_ncfile.variables['lon'][:,:]
                             wrfanl_ncfile.close()
 
                             for thres in thresholds:
                                 for rtype in region_types:
 
                                     if rtype == 'tc':
-                                        reanl_var 
+                                        best_track = os.path.join(dir_best_track, attributes[(dir_case, case_name)]['NHC_best_track'])
+                                        df = pd.read_csv(best_track)
+                                        bt_lats = list(df['LAT'][:])
+                                        bt_lons = list(df['LON'][:])
+                                        bt_dates = list(df['Date_Time'][:])
+                                        del df
+
+                                        var_time_datetime = datetime.strptime(str(var_time), '%Y%m%d%H%M%S')
+                                        for id_bt, bt_date in enumerate(bt_dates):
+                                            bt_datetime = datetime.strptime(bt_date, '%Y-%m-%d %H:%M:%S')
+                                            if bt_datetime == var_time_datetime:
+                                                bt_lat = bt_lats[id_bt]
+                                                bt_lon = bt_lons[id_bt]
+
+                                        bt_dlon = wrfanl_lon - bt_lon
+                                        bt_dlat = wrfanl_lat - bt_lat
+                                        bt_distrance_square = np.square(bt_dlon) + np.square(bt_dlat)
+                                        bt_min_pos = np.unravel_index(np.argmin(bt_distrance_square, axis=None), bt_distrance_square.shape)
+                                        tc_box_index = math.floor(tc_box_range/resolutions[idd])
+                                        reanl_var = reanl_var[bt_min_pos[0]-tc_box_index:bt_min_pos[0]+tc_box_index+1, bt_min_pos[1]-tc_box_index:bt_min_pos[1]+tc_box_index+1]
+
+                                        best_track = os.path.join(dir_best_track, '_'.join([case_name, exp_name, f"C{str(da_cycle).zfill(2)}", f"{dom}.csv"]))
+                                        if not os.path.exists(best_track):
+                                            best_track = os.path.join(dir_best_track, '_'.join([case_name, exp_name, f"C{str(da_cycle).zfill(2)}", 'd01.csv']))
+                                        df = pd.read_csv(best_track)
+                                        wrf_lats = list(df['LAT'][:])
+                                        wrf_lons = list(df['LON'][:])
+                                        wrf_dates = list(df['Date_Time'][:])
+                                        del df
+
+                                        var_time_datetime = datetime.strptime(str(var_time), '%Y%m%d%H%M%S')
+                                        for id_wrf, wrf_date in enumerate(wrf_dates):
+                                            wrf_datetime = datetime.strptime(wrf_date, '%Y-%m-%d %H:%M:%S')
+                                            if wrf_datetime == var_time_datetime:
+                                                wrf_lat = wrf_lats[id_wrf]
+                                                wrf_lon = wrf_lons[id_wrf]
+
+                                        wrf_dlon = wrfanl_lon - wrf_lon
+                                        wrf_dlat = wrfanl_lat - wrf_lat
+                                        wrf_distrance_square = np.square(wrf_dlon) + np.square(wrf_dlat)
+                                        wrf_min_pos = np.unravel_index(np.argmin(wrf_distrance_square, axis=None), wrf_distrance_square.shape)
+                                        tc_box_index = math.floor(tc_box_range/resolutions[idd])
+                                        wrfanl_var = wrfanl_var[wrf_min_pos[0]-tc_box_index:wrf_min_pos[0]+tc_box_index+1, wrf_min_pos[1]-tc_box_index:wrf_min_pos[1]+tc_box_index+1]
+                                        wrfanl_lat = wrfanl_lat[wrf_min_pos[0]-tc_box_index:wrf_min_pos[0]+tc_box_index+1, wrf_min_pos[1]-tc_box_index:wrf_min_pos[1]+tc_box_index+1]
+                                        wrfanl_lon = wrfanl_lon[wrf_min_pos[0]-tc_box_index:wrf_min_pos[0]+tc_box_index+1, wrf_min_pos[1]-tc_box_index:wrf_min_pos[1]+tc_box_index+1]
 
                                     Hit         = (reanl_var >= thres) & (wrfanl_var >= thres)
                                     False_Alarm = (reanl_var  < thres) & (wrfanl_var >= thres)
