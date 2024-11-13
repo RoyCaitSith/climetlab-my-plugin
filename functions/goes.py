8894ea9034a719de6ce6103969670eb05d2161d5
RoyCaitSith <lzfchfthdl@gmail.com>
2024-11-13 14:47:34 -0700

    update

--- functions/goes.py
+++ functions/goes.py
@@ -7,7 +7,9 @@ import importlib
 import subprocess
 import numpy as np
 import seaborn as sns
+import pyorbital.orbital as po
 import matplotlib.pyplot as plt
+from pyorbital import astronomy
 from datetime import datetime, timedelta
 from pathlib import Path
 from netCDF4 import Dataset, num2date
@@ -49,10 +51,8 @@ def create_goes_bufr_temp(dir_data, case, anl_start_time, anl_end_time, cycling_
     os.makedirs(dir_goes_bufr_temp, exist_ok=True)
     os.makedirs(dir_goes_bufr_temp_version, exist_ok=True)
 
-    interval  = 360
-    n_channel = 10
-    n_x       = 5424
-    n_y       = 5424
+    (n_channel, n_x, n_y) = (10, 5424, 5424)
+    J2000 = datetime.datetime(2000, 1,  1, 12, 0, 0, tzinfo = datetime.timezone.utc)
 
     for idc in tqdm(range(1, total_da_cycles+1), desc='Cycles', unit='files', bar_format="{desc}: {n}/{total} files | {elapsed}<{remaining}"):
 
@@ -106,138 +106,114 @@ def create_goes_bufr_temp(dir_data, case, anl_start_time, anl_end_time, cycling_
                 
                 print(file_goes.rstrip('\n'))
                 ncfile = netCDF4.Dataset(file_goes.rstrip('\n'), mode='r', format='NETCDF4')
-                CMI  = ncfile.variables['CMI'][:,:]
-                DQF  = ncfile.variables['DQF'][:,:]
-                t    = ncfile.variables['t'][:]
-                x    = ncfile.variables['x'][:]
-                y    = ncfile.variables['y'][:]
-                gip  = ncfile.variables['goes_imager_projection']
-                r_eq = gip.semi_major_axis
-                r_pol = gip.semi_minor_axis
-                H     = gip.perspective_point_height + gip.semi_major_axis
+                CMI      = ncfile.variables['CMI'][:,:]
+                DQF      = ncfile.variables['DQF'][:,:]
+                t        = ncfile.variables['t'][:]
+                x        = ncfile.variables['x'][:]
+                y        = ncfile.variables['y'][:]
+                gip      = ncfile.variables['goes_imager_projection']
+                r_eq     = gip.semi_major_axis
+                r_pol    = gip.semi_minor_axis
+                H        = gip.perspective_point_height + gip.semi_major_axis
                 phi_0    = gip.latitude_of_projection_origin
                 lambda_0 = gip.longitude_of_projection_origin
-                ncfile.close()
-                print(CMI)
-                print(DQF)
-                print(t)
-                print(x)
-                print(y)
-                print(gip)
-                print(r_eq)
-                print(r_pol)
-                print(H)
-                print(phi_0)
-                print(lambda_0)
-                print(miao)
 
-                # Extract dimensions
-                (n_bands, n_scans, n_spots) = uradl2a.variables['latitude'][:,:,:].shape
-                (n_channels, n_scans, n_spots) = uradl2a.variables['brightness_temperature'][:,:,:].shape
-                (n_vertical_levels, n_scans, n_spots) = nnavp_profiles.variables['t'][:,:,:].shape
-                n_data = n_vertical_levels*n_scans*n_spots
-                print(f"n_bands={n_bands}")
-                print(f"n_channels={n_channels}")
-                print(f"n_vertical_levels={n_vertical_levels}")
-                print(f"n_scans={n_scans}")
-                print(f"n_spots={n_spots}")
+                t     = J2000 + datetime.timedelta(seconds = t)
+                x, y  = np.meshgrid(x, y, indexing='xy')  
+                sin_x = np.sin(x)
+                cos_x = np.cos(x)
+                sin_y = np.sin(y)
+                cos_y = np.cos(y)
+                a     = np.power(sin_x, 2) + np.power(cos_x, 2)*(np.power(cos_y, 2)+np.power(r_eq*sin_y/r_pol, 2))
+                b     = -2.0*H*cos_x*cos_y
+                c     = np.power(H, 2) - np.power(r_eq, 2)
+                r_s   = (-1.0*b - np.sqrt(np.power(b, 2)-4*a*c))/(2*a)
+                s_x   = r_s*cos_x*cos_y
+                s_y   = -1.0*r_s*sin_x
+                s_z   = r_s*cos_x*sin_y
+                lat   = np.degrees(np.arctan(np.power(r_eq/r_pol, 2)*s_z/np.sqrt(np.power(H-s_x, 2)+np.power(s_y, 2))))
+                lon   = lambda_0 - np.degrees(np.arctan(s_y/(H-s_x)))
+
+                sensor_azimuth, sensor_zenith = po.get_observer_look(np.atleast_1d(lambda_0), \
+                                                                     np.atleast_1d(phi_0), \
+                                                                     np.atleast_1d((H-r_eq)/1000.0), \
+                                                                     np.atleast_1d(t), \
+                                                                     np.atleast_1d(lon), \
+                                                                     np.atleast_1d(lat), \
+                                                                     np.atleast_1d(0.0))
+                sensor_zenith  = 90.0 - sensor_zenith
+                sensor_zenith  = np.reshape(sensor_zenith, (-1))
+                sensor_azimuth = np.reshape(sensor_azimuth, (-1))
+
+                solar_zenith  = astronomy.sun_zenith_angle(t, lon, lat)
+                solar_altitude, solar_azimuth = astronomy.get_alt_az(t, lon, lat)
+                solar_azimuth = np.degrees(solar_azimuth)
+                solar_zenith  = np.reshape(solar_zenith, (-1))
+                solar_azimuth = np.reshape(solar_azimuth, (-1))
+                solar_azimuth[solar_azimuth < 0] = solar_azimuth[solar_azimuth < 0] + 360.0
 
                 # Year, Month, Day, Hour, Minute, Second
-                TROPICS_TIME = np.tile(uradl2a.variables['time'][:,:], (n_vertical_levels, 1, 1)).flatten()
-                TROPICS_DATE = num2date(TROPICS_TIME, units="seconds since 2000-01-01 00:00:00")
-                TROPICS_YEAR = np.array([date.year for date in TROPICS_DATE])
-                TROPICS_MNTH = np.array([date.month for date in TROPICS_DATE])
-                TROPICS_DAYS = np.array([date.day for date in TROPICS_DATE])
-                TROPICS_HOUR = np.array([date.hour for date in TROPICS_DATE])
-                TROPICS_MINU = np.array([date.minute for date in TROPICS_DATE])
-                TROPICS_SECO = np.array([date.second for date in TROPICS_DATE])
-                # Quality for observed position
-                TROPICS_QHDOP = np.full((n_data), 0, dtype='int')
-                # Quality for observed meteorological parameters
-                TROPICS_QHDOM = np.full((n_data), 0, dtype='int')
-                # Latitude (coarse accuracy)
-                TROPICS_CLAT = np.tile(uradl2a.variables['latitude'][0,:,:], (n_vertical_levels, 1, 1)).flatten()
-                # Longitude (coarse accuracy)
-                TROPICS_CLON = np.tile(uradl2a.variables['longitude'][0,:,:], (n_vertical_levels, 1, 1)).flatten()
-                TROPICS_CLON[TROPICS_CLON<0] = TROPICS_CLON[TROPICS_CLON<0] + 360.0
-                # Pressure (Pa)
-                TROPICS_PRLC = nnavp_profiles.variables['press'][:,:,:].flatten()
-                TROPICS_PRLC = 100.0*TROPICS_PRLC
-                # Geopotential (m2s-2)
-                TROPICS_GP10 = np.full((n_data), 0.0, dtype='float64')
-                # SDMEDIT/QUIPS quality mark for air temperature
-                TROPICS_QMAT = np.full((n_data), 1, dtype='int')
-                # Temperature/air temperature (K)
-                TROPICS_TMDB = nnavp_profiles.variables['t'][:,:,:].flatten()
-                # SDMEDIT/QUIPS quality mark for moisture
-                TROPICS_QMDD = np.full((n_data), 1, dtype='int')
-                # Specific humidity (kgkg-1)
-                TROPICS_SPFH = nnavp_profiles.variables['q'][:,:,:].flatten()
-                # SDMEDIT/QUIPS quality mark for wind (does not include NESDIS produced satellite winds)
-                TROPICS_QMWN = np.full((n_data), 2, dtype='int')
-                # Wind direction (degree)
-                TROPICS_WDIR = np.full((n_data), 0.0, dtype='float64')
-                # Wind speed (ms-1)
-                TROPICS_WSPD = np.full((n_data), 0.0, dtype='float64')
-                TROPICS_PKWDSP = np.full((n_data), 0.0, dtype='float64')
+                GOES_YEAR   = t.year
+                GOES_MNTH   = t.month
+                GOES_DAYS   = t.day
+                GOES_HOUR   = t.hour
+                GOES_MINU   = t.minute
+                GOES_SECO   = t.second + t.microsecond/1000000.0
+                GOES_CLATH  = np.reshape(lat, (-1))
+                GOES_CLONH  = np.reshape(lon, (-1))
+                GOES_SAZA   = sensor_zenith
+                GOES_SOZA   = solar_zenith
+                GOES_BEARAZ = sensor_azimuth
+                GOES_SOLAZI = solar_azimuth
+                GOES_HMSL   = H - r_eq
+                GOES_DQF    = DQF
+                GOES_TMBR07 = CMI
+                print(GOES_YEAR)
+                print(GOES_MNTH)
+                print(GOES_DAYS)
+                print(GOES_HOUR)
+                print(GOES_MINU)
+                print(GOES_SECO)
+                print(GOES_CLATH)
+                print(GOES_CLONH)
+                print(GOES_SAZA)
+                print(GOES_SOZA)
+                print(GOES_BEARAZ)
+                print(GOES_SOLAZI)
+                print(GOES_HMSL)
+                print(GOES_DQF)
+                print(GOES_TMBR07)
+                print(miao)
 
                 #Quality Control
-                bad_calQual_flag = np.tile(nnavp_masks['bad_calQual_flag'][:,:], (n_vertical_levels, 1, 1)).flatten()
-                bad_scan_flag = np.tile(nnavp_masks['bad_scan_flag'][:,:], (n_vertical_levels, 1, 1)).flatten()
-                bad_latlon = np.tile(nnavp_masks['bad_latlon'][:,:], (n_vertical_levels, 1, 1)).flatten()
-                lat_region = np.tile(nnavp_masks['lat_region'][:,:], (n_vertical_levels, 1, 1)).flatten()
-                lat_region_overlap = np.tile(nnavp_masks['lat_region_overlap'][:,:], (n_vertical_levels, 1, 1)).flatten()
-                process = np.tile(nnavp_masks['process'][:,:], (n_vertical_levels, 1, 1)).flatten()
-                land_flag = np.tile(nnavp_masks['land_flag'][:,:], (n_vertical_levels, 1, 1)).flatten()
-
-                if 'AS' in version:
-                    index = (bad_calQual_flag == 0) & (bad_scan_flag == 0) & (bad_latlon == 0) & \
-                            (lat_region == 2) & (lat_region_overlap == 0) & (process == 1) & \
-                            (np.array(TROPICS_PRLC.tolist()) != None) & \
-                            (np.array(TROPICS_TMDB.tolist()) != None) & \
-                            (np.array(TROPICS_SPFH.tolist()) != None)
-                # elif 'CS' in version:
-                #     index = (Lat_Region == 1) & (Bad_Scan_Flag == 0) & (Bad_Latlon == 0) & (Land_Flag == 0) & (Clear_Sky_Flag < 1) & \
-                #             (Lat_Region == 1) & (Bad_Scan_Flag == 0) & (Bad_Latlon == 0) & (TPW >= 0) & (TPW <= 56.5) & \
-                #             (TROPICS_CLAT <= 40.0) & (TROPICS_CLAT >= -40.0) & (TROPICS_LEVEL > 2) & \
-                #             (Bad_Qual_Flag < 2) & (Clear_Sky_Flag < 1) & (TROPICS_CLAT <= 40.0) & (TROPICS_CLAT >= -40.0) & \
-                #             (TPW >= 0.0) & (TPW <= 56.5) & \
-                #             (np.array(TROPICS_TMDB.tolist()) != None) & (np.array(TROPICS_SPFH.tolist()) != None)
-
+                index = (GOES_DQF != 0) | (np.isnan(lat)) | (np.isnan(lon))
+                index = (index == False)
                 ncfile.close()
 
                 n_data = sum(index==True)
                 print(n_data)
                 if n_data > 0:
                     n_total_data += n_data
-                    YEAR   += TROPICS_YEAR[index].tolist()
-                    MNTH   += TROPICS_MNTH[index].tolist()
-                    DAYS   += TROPICS_DAYS[index].tolist()
-                    HOUR   += TROPICS_HOUR[index].tolist()
-                    MINU   += TROPICS_MINU[index].tolist()
-                    SECO   += TROPICS_SECO[index].tolist()
-                    QHDOP  += TROPICS_QHDOP[index].tolist()
-                    QHDOM  += TROPICS_QHDOM[index].tolist()
-                    CLAT   += TROPICS_CLAT[index].tolist()
-                    CLON   += TROPICS_CLON[index].tolist()
-                    PRLC   += TROPICS_PRLC[index].tolist()
-                    GP10   += TROPICS_GP10[index].tolist()
-                    QMAT   += TROPICS_QMAT[index].tolist()
-                    TMDB   += TROPICS_TMDB[index].tolist()
-                    QMDD   += TROPICS_QMDD[index].tolist()
-                    SPFH   += TROPICS_SPFH[index].tolist()
-                    QMWN   += TROPICS_QMWN[index].tolist()
-                    WDIR   += TROPICS_WDIR[index].tolist()
-                    WSPD   += TROPICS_WSPD[index].tolist()
-                    PKWDSP += TROPICS_PKWDSP[index].tolist()
-        
-        # Calculate relative humidity
-        TROPICS_REHU = relative_humidity_from_specific_humidity(np.array(PRLC)*units.Pa, np.array(TMDB)*units.kelvin, np.array(SPFH)).to('percent').magnitude
-        TROPICS_REHU = np.array(TROPICS_REHU)
-        TROPICS_REHU[TROPICS_REHU<0] = 0.0
-        REHU = TROPICS_REHU.tolist()
-        # print(np.nanmax(REHU))
-        # print(np.nanmin(REHU))
+                    YEAR   += GOES_YEAR[index].tolist()
+                    MNTH   += GOES_MNTH[index].tolist()
+                    DAYS   += GOES_DAYS[index].tolist()
+                    HOUR   += GOES_HOUR[index].tolist()
+                    MINU   += GOES_MINU[index].tolist()
+                    SECO   += GOES_SECO[index].tolist()
+                    CLATH  += GOES_CLATH[index].tolist()
+                    CLONH  += GOES_CLONH[index].tolist()
+                    SAZA   += GOES_SAZA[index].tolist()
+                    SOZA   += GOES_SOZA[index].tolist()
+                    BEARAZ += GOES_BEARAZ[index].tolist()
+                    SOLAZI += GOES_SOLAZI[index].tolist()
+                    HMSL   += GOES_HMSL[index].tolist()
+                    TMBR07 += GOES_TMBR07[index].tolist()
+                    # TMBR08 += GOES_TMBR08[index].tolist()
+                    # TMBR09 += GOES_TMBR09[index].tolist()
+                    # TMBR10 += GOES_TMBR10[index].tolist()
+                    # TMBR11 += GOES_TMBR11[index].tolist()
+                    # TMBR12 += GOES_TMBR12[index].tolist()
+                    # TMBR13 += GOES_TMBR13[index].tolist()
 
         with open(os.path.join(dir_bufr_temp_HH,  '1.txt'), 'ab') as f:
             np.savetxt(f, YEAR)
@@ -252,35 +228,33 @@ def create_goes_bufr_temp(dir_data, case, anl_start_time, anl_end_time, cycling_
         with open(os.path.join(dir_bufr_temp_HH,  '6.txt'), 'ab') as f:
             np.savetxt(f, SECO)
         with open(os.path.join(dir_bufr_temp_HH,  '7.txt'), 'ab') as f:
-            np.savetxt(f, QHDOP)
+            np.savetxt(f, CLATH)
         with open(os.path.join(dir_bufr_temp_HH,  '8.txt'), 'ab') as f:
-            np.savetxt(f, QHDOM)
+            np.savetxt(f, CLONH)
         with open(os.path.join(dir_bufr_temp_HH,  '9.txt'), 'ab') as f:
-            np.savetxt(f, CLAT)
+            np.savetxt(f, SAZA)
         with open(os.path.join(dir_bufr_temp_HH, '10.txt'), 'ab') as f:
-            np.savetxt(f, CLON)
+            np.savetxt(f, SOZA)
         with open(os.path.join(dir_bufr_temp_HH, '11.txt'), 'ab') as f:
-            np.savetxt(f, PRLC)
+            np.savetxt(f, BEARAZ)
         with open(os.path.join(dir_bufr_temp_HH, '12.txt'), 'ab') as f:
-            np.savetxt(f, GP10)
+            np.savetxt(f, SOLAZI)
         with open(os.path.join(dir_bufr_temp_HH, '13.txt'), 'ab') as f:
-            np.savetxt(f, QMAT)
+            np.savetxt(f, HMSL)
         with open(os.path.join(dir_bufr_temp_HH, '14.txt'), 'ab') as f:
-            np.savetxt(f, TMDB)
+            np.savetxt(f, TMBR07)
         with open(os.path.join(dir_bufr_temp_HH, '15.txt'), 'ab') as f:
-            np.savetxt(f, QMDD)
+            np.savetxt(f, TMBR08)
         with open(os.path.join(dir_bufr_temp_HH, '16.txt'), 'ab') as f:
-            np.savetxt(f, SPFH)
+            np.savetxt(f, TMBR09)
         with open(os.path.join(dir_bufr_temp_HH, '17.txt'), 'ab') as f:
-            np.savetxt(f, REHU)
+            np.savetxt(f, TMBR10)
         with open(os.path.join(dir_bufr_temp_HH, '18.txt'), 'ab') as f:
-            np.savetxt(f, QMWN)
+            np.savetxt(f, TMBR11)
         with open(os.path.join(dir_bufr_temp_HH, '19.txt'), 'ab') as f:
-            np.savetxt(f, WDIR)
+            np.savetxt(f, TMBR12)
         with open(os.path.join(dir_bufr_temp_HH, '20.txt'), 'ab') as f:
-            np.savetxt(f, WSPD)
-        with open(os.path.join(dir_bufr_temp_HH, '21.txt'), 'ab') as f:
-            np.savetxt(f, PKWDSP)
+            np.savetxt(f, TMBR13)
         np.savetxt(os.path.join(dir_bufr_temp_HH, '0.txt'), [n_total_data])
         print('\n')
 
